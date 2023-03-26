ffbaa2d90fe451e1897ad48f652f69bdcc415b17
Irek-91 <shamilov.irek@bk.ru>
2023-03-26 13:26:54 +0300

    Функция расчета возрастов

---
+++ Cat years, Dog years.js
@@ -0,0 +1,15 @@
+function(humanYears) {
+    let catYears = 0;
+    let dogYears = 0;
+        if (humanYears === 1) {
+        catYears = 15;
+        dogYears = 15;
+    } else if (humanYears === 2) {
+        catYears = 24;
+        dogYears = 24;
+    } else if (humanYears > 2) {
+        catYears = 24 + (humanYears - 2) * 4;
+        dogYears = 24 + (humanYears - 2) * 5;
+    }
+return [humanYears, catYears, dogYears];
+}
