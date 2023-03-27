86c262cf93f5a68d0afabd1c8a1cc71ed5913595
Irek-91 <shamilov.irek@bk.ru>
2023-03-27 23:57:02 +0300

    Ближайшее квадратное число, из целого положительного числа n (скорректировали функцию if)

--- FindNearestSquareNumber.js
+++ FindNearestSquareNumber.js
@@ -3,8 +3,5 @@ let n1;
 let n2;
 n1 = (Math.floor(n ** 0.5)) ** 2;
 n2 = (Math.ceil(n ** 0.5)) ** 2;
-if ((n2 - n) < (n - n1)) {
-    return n2} else {
-    return n1}
-    // your code
+return ((n2 - n) < (n - n1)) ? n2 : n1;
 }
