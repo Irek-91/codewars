288c28027f9ec5881bcea596579388c457cb5752
Irek-91 <shamilov.irek@bk.ru>
2023-03-29 20:01:34 +0300

    Even or Odd

---
+++ EverOrOdd.js
@@ -0,0 +1,10 @@
+//функция определяет четные или нечентные числа
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {return "Even"} else {return "Odd"}
+    }
+
+//сокращенно
+function even_or_odd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+}
